5736b3f69ebc0aa8bb3ed18a126792fdac362a88
JULIIÁN LIFSCHITZ <47575674@ort>
2023-03-30 09:09:45 -0300

    mi primer commit

---
+++ Persona.cs
@@ -0,0 +1,21 @@
+﻿class persona
+{
+public int dni{get;set;}
+public string apellido{get;set;}
+public string nombre{get;set;}
+public DateTime fechaN{get;set;}
+public string mail{get;set;}
+
+public int ObtenerEdad(DateTime fechaN)
+{
+    int devolver;
+    if (fechaN < DateTime.Today) devolver = DateTime.Today.Year - fechaN.Year;
+    else devolver = DateTime.Today.Year - fechaN.Year -1;
+    return devolver;
+}
+public bool PuedeVolar(int edad)
+{
+    if (edad >= 16) return true;
+    else return false;
+}
+}
---
+++ funciones.cs
@@ -0,0 +1,7 @@
+static string IngresarString(string txt)
+{
+    string devolver;
+    Console.WriteLine(txt);
+    devolver = Console.ReadLine();
+    return devolver;
+}
